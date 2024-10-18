f0970f9fe08075eb0948a26d8f754278ff36a445
Ashek Mahmud <ashikeu@gmail.com>
2024-10-18 12:25:56 +0600

    initialize

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_cart/screen/product_list.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: ProductList(),
+  ));
+}
---
+++ lib/screen/product_list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProductList extends StatelessWidget {
+  const ProductList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: Text('Product Page'),
+      ),
+      body: Container(),
+    );
+  }
+}
